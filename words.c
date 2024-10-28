aac6b64e4f75bd9f6ce01efb823d51aa2a299398
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2024-10-27 21:48:52 -0400

    Updated process_file

--- words.c
+++ words.c
@@ -8,14 +8,14 @@
 
 /* TODO add argument for arraylist/BST */
 void process_file(char *pathname) {
-	struct stat *stat_data;
-	int r = stat(pathname, stat_data);
+	struct stat stat_data;
+	int r = stat(pathname, &stat_data);
 	if (r != 0) {
 		perror(pathname);
 	} else if (pathname[0] != '.') {
-		if (S_ISREG(stat_data->st_mode) && (strcmp(".txt", pathname + strlen(pathname) - strlen(".txt")) == 0)) {
+		if (S_ISREG(stat_data.st_mode) && (strcmp(".txt", pathname + strlen(pathname) - strlen(".txt")) == 0)) {
 			count_words(pathname);
-	    } else if (S_ISDIR(stat_data->st_mode)) {
+	    } else if (S_ISDIR(stat_data.st_mode)) {
 		    DIR *dirp = opendir(pathname);
 		    if (dirp == NULL) {
 		    	perror(pathname);
@@ -27,6 +27,7 @@ void process_file(char *pathname) {
 					strcat(new_path, "/");
 				    strcat(new_path, direntp->d_name);
 				    process_file(new_path);
+					free(new_path)
 			    }
 				r = closedir(dirp);
 				if (r != 0) {
